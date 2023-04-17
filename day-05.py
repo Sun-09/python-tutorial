f4aef0e1d8ca546cc1987dec5289d27b88e13836
Souradeep <souradeepbera162@gmail.com>
2023-04-18 01:24:50 +0530

    3rd commit

---
+++ day-05.py
@@ -0,0 +1,21 @@
+# TYPECASTING IN PYTHON
+
+a = "1"
+b= "2"
+
+print(a+b)  #As a and b are strings so the output is 12
+
+# EXPLICIT TYPECASTING 
+
+a = int(a)  # typecasting - converting string to integar
+b = int(b)  # typecasting - converting string to integar
+
+print(a+b)  # now a and b are integar
+
+
+# IMPLICIT TYPECASTING
+
+c = 1.9
+d = 2
+
+print(c+d)
---
+++ day-06.py
@@ -0,0 +1,17 @@
+# Using INPUT FUNCTION IN PYTHON
+
+a = input("Enter your name - ")  #taking input from user as string
+print("My name is - ", a)
+
+
+# IF YOU WANT TO TAKE INTEGAR YOU HAVE TO TYPECAST
+
+x = input("Enter first number : ")   
+y = input("Enter first number : ")
+
+print(x+y)   #OUTPUT :- xy
+
+print(int(x) + int(y))  #OUTPUT :- x+y
+
+
+
