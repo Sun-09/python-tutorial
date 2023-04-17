aabcebbac4efb086621c16ccb1cf20a88b121798
Souradeep <souradeepbera162@gmail.com>
2023-04-17 18:17:59 +0530

    first commit

---
+++ day-01.py
@@ -0,0 +1,4 @@
+print("hello world")
+print(5)
+print('Bye', 6)
+print(17*13)
---
+++ day-02.py
@@ -0,0 +1,29 @@
+ # Below lines and this line is a commment statement. Python will skip this 
+# lines.It will execute the pogram from 8th line. You can comment any statement \
+# by 'ctrl + /' or by using '#'
+# Author:Sonu
+# Corse: 100DaysofCode by 'Code with Harry'.
+# Using Escape Sequence Character
+
+print("Hey I am good boy\nand this viewer is also a very\ngood boy/girl")
+
+print("Hello World")  #Comment can be use in same line with code
+
+# Commenting Hello World -
+
+# print("Hello World")
+
+'''Commenting can be done
+by tripple double/single qoute. It is used in multi-line
+comments'''
+
+print("I Really like - \"Dark Knight Triology\" series")  # Using \"" to ignore double quote.....
+
+print("Hey", 6, 7)  #Printing Multiple values
+
+print("Hey", 6, 7, sep='/')  #Using Separator
+
+print("Hey", "Hello", '', end='welcome')
+print('',"to the show")
+
+
